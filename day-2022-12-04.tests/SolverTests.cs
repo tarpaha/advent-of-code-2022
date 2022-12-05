c4bbd5490bdc9eb065c9b5f412b9a427658a5b49
tarpaha <ppopchikovsky@gmail.com>
2022-12-05 14:36:25 +0300

    day-2022-12-04 part1

--- day-2022-12-04.tests/SolverTests.cs
+++ day-2022-12-04.tests/SolverTests.cs
@@ -2,12 +2,18 @@ namespace day_2022_12_04.tests;
 
 public class SolverTests
 {
-    private const string Data = @"";
+    private const string Data = @"
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
 
     [Test]
     public void Part1()
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(2));
     }
     
     [Test]
--- day-2022-12-04/Solver.cs
+++ day-2022-12-04/Solver.cs
@@ -4,7 +4,14 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        return null!;
+        var result = 0;
+        foreach (var (first, second) in data.Pairs)
+        {
+            if ((first.Min <= second.Min && first.Max >= second.Max) ||
+                (second.Min <= first.Min && second.Max >= first.Max))
+                result += 1;
+        }
+        return result;
     }
 
     public static object Part2(Data data)
