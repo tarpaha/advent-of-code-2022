5866c239d2b708824d5cf3879f7e736cae2a2f4f
tarpaha <ppopchikovsky@gmail.com>
2022-12-05 17:14:55 +0300

    day-2022-12-05 part1

--- day-2022-12-05.tests/SolverTests.cs
+++ day-2022-12-05.tests/SolverTests.cs
@@ -2,12 +2,21 @@ namespace day_2022_12_05.tests;
 
 public class SolverTests
 {
-    private const string Data = @"";
+    private const string Data = @"
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
 
     [Test]
     public void Part1()
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo("CMZ"));
     }
     
     [Test]
--- day-2022-12-05/Solver.cs
+++ day-2022-12-05/Solver.cs
@@ -4,7 +4,13 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        return null!;
+        var stacks = data.Stacks.Select(stack => new Stack<char>(stack.Crates)).ToList();
+        foreach (var move in data.Moves)
+        {
+            for(var step = 0; step < move.Count; step++)
+                stacks[move.To - 1].Push(stacks[move.From - 1].Pop());
+        }
+        return string.Join("", stacks.Select(stack => stack.Pop()));
     }
 
     public static object Part2(Data data)
