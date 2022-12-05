7b1019dd0a7180c100f0f776317412a700a6f2e8
tarpaha <ppopchikovsky@gmail.com>
2022-12-05 16:06:50 +0300

    day-2022-12-04 part2

--- day-2022-12-04/Solver.cs
+++ day-2022-12-04/Solver.cs
@@ -16,6 +16,15 @@ public static class Solver
 
     public static object Part2(Data data)
     {
-        return null!;
+        var result = 0;
+        foreach (var (first, second) in data.Pairs)
+        {
+            if ((second.Min <= first.Min && first.Min <= second.Max) ||
+                (second.Min <= first.Max && first.Max <= second.Max) ||
+                (first.Min <= second.Min && second.Min <= first.Max) ||
+                (first.Min <= second.Max && second.Max <= first.Max))
+                result += 1;
+        }
+        return result;
     }
 }
