e21b582bde0f4b0888607b9dac704d97acb367b3
tarpaha <ppopchikovsky@gmail.com>
2022-12-06 11:07:50 +0300

    day-2022-12-06 part2

--- day-2022-12-06/Solver.cs
+++ day-2022-12-06/Solver.cs
@@ -12,13 +12,54 @@ public static class Solver
         return MarkerPosition(data.Characters.ToArray(), 14);
     }
 
-    private static int MarkerPosition(char[] chars, int markerSize)
+    private static int MarkerPositionNaive(char[] chars, int markerSize)
     {
         for (var p = 0; p < chars.Length; p++)
         {
             if (new HashSet<char>(chars[p..(p + markerSize)]).Count == markerSize)
                 return p + markerSize;
         }
+        throw new ArgumentException();
+    }
+    
+    private static int MarkerPosition(char[] chars, int markerSize)
+    {
+        var charsCount = new int['z' - 'a' + 1];
+        var onesCount = 0;
+        
+        for (var i = 0; i < chars.Length; i++)
+        {
+            if (i >= markerSize)
+            {
+                var lch = chars[i - markerSize];
+                charsCount[lch - 'a'] -= 1;
+                switch (charsCount[lch - 'a'])
+                {
+                    case 0:
+                        onesCount -= 1;
+                        break;
+                    case 1:
+                        onesCount += 1;
+                        break;
+                }
+            }
+
+            var rch = chars[i];
+            charsCount[rch - 'a'] += 1;
+            switch (charsCount[rch - 'a'])
+            {
+                case 1:
+                    onesCount += 1;
+                    break;
+                case 2:
+                    onesCount -= 1;
+                    break;
+            }
+            
+            if(onesCount == markerSize)
+                return i + 1;
+        }
+        
         throw new ArgumentException();
     }
 }
