26f2d1b0bc999684154fea328587d704cac7c1b3
tarpaha <ppopchikovsky@gmail.com>
2022-12-07 16:45:01 +0300

    file system with tests

---
+++ day-2022-12-07.tests/FileSystemTests.cs
@@ -0,0 +1,51 @@
+﻿namespace day_2022_12_07.tests;
+
+public class FileSystemTests
+{
+    private const string Data = @"
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+    
+    [Test]
+    public void FileSystem_Creates_Properly()
+    {
+        var root = FileSystem.CreateFrom(Parser.Parse(Data));
+        Assert.That(Environment.NewLine + root, Is.EqualTo(@"
+- / (dir)
+  - a (dir)
+    - e (dir)
+      - i (file, size=584)
+    - f (file, size=29116)
+    - g (file, size=2557)
+    - h.lst (file, size=62596)
+  - b.txt (file, size=14848514)
+  - c.dat (file, size=8504156)
+  - d (dir)
+    - d.ext (file, size=5626152)
+    - d.log (file, size=8033020)
+    - j (file, size=4060174)
+    - k (file, size=7214296)
+"));
+    }
+}
---
+++ day-2022-12-07/FileSystem.cs
@@ -0,0 +1,88 @@
+﻿namespace day_2022_12_07;
+
+public class DirectoryEntry
+{
+    public string Name { get; }
+    public long Size { get; }
+    public DirectoryEntry? Parent { get; }
+
+    public bool IsDir => Size == 0;
+    
+    private readonly List<DirectoryEntry> _entries = new();
+    
+    public DirectoryEntry(DirectoryEntry? parent, string name, long size = 0)
+    {
+        Name = name;
+        Size = size;
+        Parent = parent;
+    }
+
+    public DirectoryEntry AddAndReturnEntryByName(string name, long size = 0)
+    {
+        var entry = _entries.Find(entry => entry.Name == name);
+        if (entry != null)
+            return entry;
+        entry = new DirectoryEntry(this, name, size);
+        _entries.Add(entry);
+        return entry;
+    }
+
+    public override string ToString() => ToString("");
+    private string ToString(string indent)
+    {
+        var str = indent + $"- {Name} (" + (IsDir ? "dir" : $"file, size={Size}")  + ")" + Environment.NewLine;
+        foreach (var entry in _entries.OrderBy(entry => entry.Name))
+        {
+            str += entry.ToString(indent + "  ");
+        }
+        return str;
+    }
+}
+
+public static class FileSystem
+{
+    public static DirectoryEntry CreateFrom(Data data)
+    {
+        var root = new DirectoryEntry(null, "/");
+        var current = root;
+        foreach (var record in data.Records)
+        {
+            switch (record)
+            {
+                case Record.CommandRecord({ } command):
+                    switch (command)
+                    {
+                        case Command.ListContentCommand:
+                            break;
+                        case Command.ChangeDirectoryCommand({ } changeDirectory):
+                            switch (changeDirectory)
+                            {
+                                case ChangeDirectory.Root:
+                                    current = root;
+                                    break;
+                                case ChangeDirectory.Up:
+                                    current = current?.Parent;
+                                    break;
+                                case ChangeDirectory.Down({ } name):
+                                    current = current?.AddAndReturnEntryByName(name);
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case Record.InfoRecord({ } info):
+                    switch (info)
+                    {
+                        case Info.DirectoryInfo directoryInfo:
+                            current?.AddAndReturnEntryByName(directoryInfo.Name);
+                            break;
+                        case Info.FileInfo fileInfo:
+                            current?.AddAndReturnEntryByName(fileInfo.Name, fileInfo.Size);
+                            break;
+                    }
+                    break;
+            }
+        }
+        return root;
+    }
+}
