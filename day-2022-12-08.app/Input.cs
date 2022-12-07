9043dfdae776e26f6a8d31a123eb0a704c9874c8
tarpaha <ppopchikovsky@gmail.com>
2022-12-07 23:43:48 +0300

    day-2022-12-08 start

---
+++ day-2022-12-08.app/Input.cs
@@ -0,0 +1,9 @@
+namespace day_2022_12_08.app;
+
+public static class Input
+{
+    public static string GetData()
+    {
+        return @"";
+    }
+}
---
+++ day-2022-12-08/Data.cs
@@ -0,0 +1,5 @@
+namespace day_2022_12_08;
+
+public class Data
+{
+}
