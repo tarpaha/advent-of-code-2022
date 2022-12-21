ec400937dba13491e2034a17fd753f7a644387e5
tarpaha <ppopchikovsky@gmail.com>
2022-12-21 22:09:21 +0300

    day-2022-12-14 part1

--- day-2022-12-14.tests/SolverTests.cs
+++ day-2022-12-14.tests/SolverTests.cs
@@ -2,12 +2,14 @@ namespace day_2022_12_14.tests;
 
 public class SolverTests
 {
-    private const string Data = @"";
+    private const string Data = @"
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9";
 
     [Test]
     public void Part1()
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(24));
     }
     
     [Test]
--- day-2022-12-14/Solver.cs
+++ day-2022-12-14/Solver.cs
@@ -4,7 +4,11 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        return null!;
+        var cave = new Cave(data, 500, 0);
+        var amount = 0;
+        while (cave.DropSand())
+            amount += 1;
+        return amount;
     }
 
     public static object Part2(Data data)
